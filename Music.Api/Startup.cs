6fff2f89cafb931db6aedb38ec572e3da94a7698
Pyotr <2chilavert3@mail.ru>
2020-12-05 21:27:39 +0300

    Создание БД, схемы и заполнение данными.

--- Music.Api/Startup.cs
+++ Music.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +28,13 @@ namespace Music.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services                
+            var connectionString = Configuration.GetConnectionString("Default");
+            services               
+                .AddDbContext<MusicDbContext>(options => 
+                    options.UseSqlServer(
+                        connectionString,
+                        builder => builder.MigrationsAssembly("Music.Data"))
+                )
                 .AddScoped<IUnitOfWork, UnitOfWork>()
                 .AddControllers();
         }
