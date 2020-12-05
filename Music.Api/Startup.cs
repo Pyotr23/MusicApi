3d5e40193e21dcea6dda7f9ff4c15a1cfcfd9ef5
Pyotr <2chilavert3@mail.ru>
2020-12-05 20:51:12 +0300

    Добавлен класс UnitOfWork.

--- Music.Api/Startup.cs
+++ Music.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Music.Core;
+using Music.Data;
 
 namespace Music.Api
 {
@@ -25,7 +27,9 @@ namespace Music.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services                
+                .AddScoped<IUnitOfWork, UnitOfWork>()
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Music.Data/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Music.Core;
+using Music.Core.Repositories;
+using Music.Data.Repositories;
+using System.Threading.Tasks;
+
+namespace Music.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MusicDbContext _context;
+        private SongRepository _songRepository;
+        private ArtistRepository _artistRepository;
+
+        public UnitOfWork(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        public ISongRepository Songs => _songRepository ??= new SongRepository(_context);
+        public IArtistRepository Artists => _artistRepository ??= new ArtistRepository(_context);
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
