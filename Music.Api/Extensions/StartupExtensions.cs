10d55a0a34701f3cb7c808236dca3a3edd061281
Pyotr <2chilavert3@mail.ru>
2020-12-07 22:00:38 +0300

    Добавлен сервис Artist.

---
+++ Music.Api/Extensions/StartupExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Music.Api.Configurations;
+
+namespace Music.Api.Extensions
+{
+    internal static class StartupExtensions
+    {
+        internal static ServiceProvider AddCustomOptions(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var apiConfigurationSectionName = configuration.GetSection(nameof(ApiConfiguration));
+            return services
+                .Configure<ApiConfiguration>(apiConfigurationSectionName)
+                .BuildServiceProvider();
+        }
+    }
+}
--- Music.Api/Startup.cs
+++ Music.Api/Startup.cs
@@ -11,8 +11,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Music.Api.Configurations;
+using Music.Api.Extensions;
 using Music.Core;
+using Music.Core.Services;
 using Music.Data;
+using Music.Services;
 
 namespace Music.Api
 {
@@ -28,19 +34,28 @@ namespace Music.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var apiConfiguration = getApiConfiguration(services);
+            services
+                .AddSwaggerGen(options =>
+                {
+                    options.SwaggerDoc(apiConfiguration.Version, (OpenApiInfo)apiConfiguration);
+                });
+
             var connectionString = Configuration.GetConnectionString("Default");
             services               
                 .AddDbContext<MusicDbContext>(options => 
                     options.UseSqlServer(
                         connectionString,
                         builder => builder.MigrationsAssembly("Music.Data"))
-                )
+                )                
+                .AddTransient<ISongService, SongService>()  
+                .AddTransient<IArtistService, ArtistService>()
                 .AddScoped<IUnitOfWork, UnitOfWork>()
                 .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<ApiConfiguration> apiOption)
         {
             if (env.IsDevelopment())
             {
@@ -53,10 +68,26 @@ namespace Music.Api
 
             app.UseAuthorization();
 
+            var apiConfiguration = apiOption.Value;
+            app.UseSwagger(); 
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", apiConfiguration.Name);
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private ApiConfiguration getApiConfiguration(IServiceCollection services)
+        {
+            return services
+                .AddCustomOptions(Configuration)
+                .GetRequiredService<IOptions<ApiConfiguration>>()
+                .Value;
         }
     }
 }
